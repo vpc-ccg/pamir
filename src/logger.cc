03e9a360772682721c11a735e0eddc516b06b28e
Faraz Hach <faraz.hach@ubc.ca>
2019-11-15 15:16:34 -0800

    Logger code reformat

--- src/logger.cc
+++ src/logger.cc
@@ -11,43 +11,46 @@ FILE *output;
 char *buffer;
 int buffer_size;
 
-void log_init(string name = "")
-{
-	buffer = new char[20*1024*1024];
-	buffer[0] = '\0';
-	buffer_size = 0;
-	if (name != "")
-		output = fopen(name.c_str(), "w");
-	else
-		output = stdout;
+/**
+ * Logger Initiliazer
+ * @param name Optional location of Log file
+ */
+void log_init(string name = "") {
+    buffer = new char[20 * 1024 * 1024];
+    buffer[0] = '\0';
+    buffer_size = 0;
+    if (name != "")
+        output = fopen(name.c_str(), "w");
+    else
+        output = stdout;
 }
 
-
-void log_close()
-{
-	buffer[buffer_size]='\0';
-	fwrite (buffer , sizeof(char), buffer_size, output);
-	//fprintf(output, "%s", buffer);
-	if (output != stdout)
-		fclose(output);
-	delete buffer;
+/**
+ * Logger closer
+ */
+void log_close() {
+    buffer[buffer_size] = '\0';
+    fwrite(buffer, sizeof(char), buffer_size, output);
+    if (output != stdout)
+        fclose(output);
+    delete buffer;
 }
 
-void log(const char* format, ...)
-{
-	va_list args;
-	va_start (args, format);
-	buffer_size += vsprintf(buffer+buffer_size, format, args);
-	va_end(args);
-	if (buffer_size > 5200000) 
-	{
-		buffer[buffer_size]='\0';
-		fwrite (buffer , sizeof(char), buffer_size, output);
-		//fprintf(output, "%s", buffer);
-		fflush(output);
-		buffer_size=0;
-		buffer[0] = '\0';
-	}
+/**
+ * Logger command to log the given input
+ * @param format
+ * @param ...
+ */
+void log(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    buffer_size += vsprintf(buffer + buffer_size, format, args);
+    va_end(args);
+    if (buffer_size > 5 * 1024 * 1024) {
+        fwrite(buffer, sizeof(char), buffer_size, output);
+        fflush(output);
+        buffer_size = 0;
+    }
 }
 
 
