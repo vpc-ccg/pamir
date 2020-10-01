974bec3808c3bf22e706a013c0e1fd1a20efef88
Armita <armita@alumni.ubc.ca>
2020-10-01 18:23:38 +0330

    added adjusted partition file for pamir extract output

---
+++ src/include/p3_partition.h
@@ -0,0 +1,55 @@
+#ifndef __PARTITION_P3__
+#define __PARTITION_P3__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+#include <zlib.h>
+#include <unordered_set>
+using namespace std;
+
+class p3_partition {
+private:
+    FILE* partition_out_file;
+    FILE* partition_out_index_file;
+    FILE* partition_out_count_file;
+
+    int partition_count = 0;
+    int partition_id = 1;
+
+    int p_start;
+    int p_end;
+    string p_ref;
+
+    int start;
+    int end;
+    FILE* partition_file;
+
+	int old_id;
+
+    vector<pair<pair<string, string>, pair<int,int> > > current_cluster;
+
+
+	char prev_string[2000];
+
+	const int INSSIZE=450;
+	
+public:
+	p3_partition (const string&, bool write_to_file = false);
+	p3_partition (const string&,  const string&);
+	~p3_partition (void);
+
+    void add_reads(vector<pair<pair<string, string>, pair<int,int> > > cuts, int p_start, int p_end, string p_ref, int pt_id);
+	vector<pair<pair<string, string>, pair<int,int> > > read_partition ();
+
+	int get_start (void);
+	int get_end (void);
+	string get_reference (void);
+	int get_id ();
+	int get_old_id();
+};
+
+
+#endif
---
+++ src/p3_partition.cc
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <cstdio>
+#include <cassert>
+#include <zlib.h>
+#include <cstring>
+#include <cstdlib>
+#include "common.h"
+#include "p3_partition.h"
+#define MAXB  809600
+
+using namespace std;
+
+//OPEN WRITE FILE
+p3_partition::p3_partition (const string & out_prefix, bool write_to_files): partition_out_file(NULL), partition_out_index_file(NULL), partition_out_count_file(NULL), partition_file(NULL) {
+    if(write_to_files){
+        partition_out_file = fopen(out_prefix.c_str(), "wb");
+        if (partition_out_file == NULL){
+            throw("[Genome Partition] Cannot open the file.");
+        }
+        partition_out_index_file = fopen((out_prefix + ".idx").c_str(), "wb");
+        if (partition_out_index_file == NULL){
+            throw("[Genome Partition] Cannot open the file.");
+        }
+        partition_out_count_file = fopen((out_prefix + ".count").c_str(), "w");
+        if (partition_out_count_file == NULL){
+            throw("[Genome Partition] Cannot open the file.");
+        }
+    }
+}
+
+//READ
+p3_partition::p3_partition (const string &partition_file_path, const string &range): partition_out_file(NULL), partition_out_index_file(NULL), partition_out_count_file(NULL), partition_file(NULL) {
+    // extracting range [start,end]
+    size_t pos;
+    start = stoi (range, &pos);
+    if (pos < range.size()) {
+        end = stoi (range.substr(pos+1));
+    } else {
+        end = start;
+    }
+
+    // reading the index file of partitions
+    FILE *partition_file_index = fopen((partition_file_path + ".idx").c_str(), "rb");
+    if ( partition_file_index == NULL)
+        throw("[Genome Partition] Cannot open the file.");
+
+    Logger::instance().info ("Loading the index file.\n");
+    vector<size_t> offsets;
+    size_t offset;
+    while (fread(&offset, 1, sizeof(size_t), partition_file_index) == sizeof(size_t))
+    {
+        offsets.push_back(offset);
+    }
+
+    fclose(partition_file_index);
+
+    if (start < 1)
+        start = 1;
+    if ( end > offsets.size()+1 )
+        end = offsets.size()+1;
+
+    partition_file = fopen(partition_file_path.c_str(), "rb");
+    fseek(partition_file, offsets[start-1], SEEK_SET);
+}
+
+//CALL ON EACH CLUSTER
+void p3_partition::add_reads(vector<pair<pair<string, string>, pair<int,int> > > cuts, int p_start, int p_end, string p_ref, int pt_id) {
+    size_t pos = ftell(partition_out_file);
+    fwrite(&pos, 1, sizeof(size_t), partition_out_index_file);
+    fprintf(partition_out_file, "%d %lu %d %d %s %d\n", partition_id, cuts.size(), p_start, p_end, p_ref.c_str(), pt_id);
+	for (auto &i: cuts) {
+		fprintf(partition_out_file, "%s %s %d %d\n", i.first.first.c_str(), i.first.second.c_str(), i.second.first, i.second.second);
+	}
+	partition_id++;
+}
+
+p3_partition::~p3_partition (){
+    if (partition_file != NULL){
+        fclose(partition_file);
+    }
+    if (partition_out_file != NULL){
+        fclose (partition_out_file);
+    }
+    if (partition_out_index_file != NULL){
+        fclose (partition_out_index_file);
+    }
+    if (partition_out_count_file != NULL){
+        fprintf(partition_out_count_file, "%d\n", partition_count);
+        fclose (partition_out_count_file);
+    }
+}
+
+int p3_partition::get_start ()
+{
+	return p_start;
+}
+
+int p3_partition::get_end ()
+{
+	return p_end;
+}
+
+string p3_partition::get_reference ()
+{
+	return p_ref;
+}
+
+int p3_partition::get_id () {
+	return partition_id;
+}
+
+int p3_partition::get_old_id () {
+	return old_id;
+}
+
+//read next partition
+vector<pair<pair<string, string>, pair<int,int> > > p3_partition::read_partition() {
+	int cut_sz, i;
+	char pref[MAXB];
+	char name[MAXB], read[MAXB];
+
+	if (start >= end)
+		return vector < pair < pair < string, string >, pair < int, int >> > ();
+
+	partition_count++;
+	start++;
+	fscanf(partition_file, "%d %d %d %d %s %d\n", &partition_id, &cut_sz, &p_start, &p_end, pref, &old_id);
+	p_ref = pref;
+	current_cluster.resize(0);
+	current_cluster.reserve(cut_sz);
+
+	for (i = 0; i < cut_sz; i++) {
+		fgets(pref, MAXB, partition_file);
+		int start_pos, end_pos;
+		sscanf(pref, "%s %s %d %d", name, read, &start_pos, &end_pos);
+		current_cluster.push_back({{string(name), string(read)}, {start_pos, end_pos}});
+	}
+	return current_cluster;
+}
+
+
